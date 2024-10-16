be05cca2fc4e082c1800bf1c3077e5a4c8c5cd89
César Kauan Barbosa Silva <cesarkauan81@gmail.com>
2024-10-16 15:58:56 -0300

    Create atividade1.js

---
+++ atividade1.js
@@ -0,0 +1,83 @@
+//Atividades referentes as aulas de CODING (javascript)
+
+/*
+//Declare uma variavel chamada NOME (coloque o seu nome)
+Declare uma variavel chamada IDADE (coloque a sua idade)
+imprima na tela uma mensagem usando as duas variaveis numa frase
+
+const nome = "César"
+const idade = 20
+console.log(`Olá meu nome é ${nome} e tenho ${idade} anos!`)
+*/
+
+/*
+//Declare uma variavel para nome e para cidade e armazene informações do tipo string em variaveis e imprima uma mensagem personalizada
+
+const NomeDaCidade = "Olinda"
+const nome = "César"
+console.log(`Olá meu nome é ${nome} e atualmente estou morando em ${NomeDaCidade}`)
+
+/*
+
+//Declare duas variasveis num1 e num2,faça quatro operações basicas (+,-,*,/) e imprima os resultados
+
+/*
+const numero1 = 76
+const numero2 = 33
+const subtracao = numero1 - numero2
+const adicao = numero1 + numero2
+const multiplicacao = numero1 * numero2 
+const divisao = numero1 / numero2
+console.log(`calcule o valor da adicao:${adicao}`);
+console.log(`calcule o valor da divisao:${divisao}`);
+console.log(`calcule o valor da multiplicacao:${multiplicacao}`);
+console.log(`calcule o valor da subtracao:${subtracao}`); 
+*/
+
+
+//Faça variaveis para Base e Altura,armazene e calcule a area e exiba o resultado
+
+/*
+let Base = 25
+let Altura = 10
+let  Area = Base * Altura
+console.log(`A área do Triangulo é ${Area}`)
+*/
+
+//Calculo de media,declare duas variaveis numericas:base e altura calcule a area e exiba-a
+
+/*
+let nota1 = 6.5
+let nota2 = 7.8
+let nota3 = 9.0
+let media = ((nota1 + nota2 + nota3) /3)
+console.log(`A media do Aluno é ${media}`)
+*/
+
+/*
+//Crie um programa que calcule o valor final de um produto com desconto,as variaveis devem informar o preço original e o percentual de desconto,no final deve exibir o resultado com uma mesagem personalizada
+
+let precoOriginal = 256
+let percentualDesconto = 15
+
+let desconto = precoOriginal * (percentualDesconto / 100);
+
+let precoFinal = precoOriginal - desconto;
+
+console.log(`O preço final com desconto é: R$ ${precoFinal.toFixed(2)}`);
+*/
+
+//Cambio de moedas
+/*
+let valor_reais = 100
+let cotacao_dolar = 5.57
+let valor_dolares = valor_reais/cotacao_dolar
+console.log(`${valor_reais} R$ são equivalentes à: ${valor_dolares} US$`)
+*/
+
+//Conversor de temperaturas
+/*
+let temperatura_celsius = 28
+let temperatura_fahrenheit = (temperatura_celsius*9/5) + 32
+console.log(`${temperatura_celsius} graus celsius são equivalentes à: ${temperatura_fahrenheit} graus fahrenheit`);
+*/
